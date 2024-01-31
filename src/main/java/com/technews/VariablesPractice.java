52202105a3a2177376f6fe9b32066363c6e56503
jortega13 <juanpo1313@yahoo.com>
2024-01-30 20:09:42 -0600

    meoed

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,14 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int car = 1;
+        double car2 = 3.5;
+        double sum = car + car2;
+        String pito = "Siuu";
+        String sum2 = pito + " " + car;
+
+        System.out.println(sum);
+        System.out.println(sum2);
+    }
+}
